7b4a74c6da8c03d6b251dc8955fe0864fadb6ec4
Daniel <buh.add@gmail.com>
2017-07-29 09:51:49 +0000

    Configure admin

--- bids_app/admin.py
+++ bids_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from bids_app.models import Channel, Campaign
 
-# Register your models here.
+
+class CampaignInline(admin.StackedInline):
+    model = Campaign
+
+class ChannelAdmin(admin.ModelAdmin):
+    model = Channel
+    inlines = [CampaignInline]
+
+
+admin.site.register(Channel, ChannelAdmin)
+admin.site.register(Campaign)
